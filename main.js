c9a0851521c0b70255f1a6d13fdb7b164868451d
Bia Grigore <biancag.grigore@gmail.com>
2024-03-23 17:48:23 +0200

    lucrat la shop, signin, sign up, about us

--- main.js
+++ main.js
@@ -26,6 +26,16 @@ let search = () => {
 };
 
 //
+function openNav() {
+  document.getElementById("mySidebar").style.width = "420px";
+  document.getElementById("main").style.marginLeft = "400px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+//
 
 //
 
--- shop.js
+++ shop.js
@@ -1,4 +1,4 @@
-let countDownDate = new Date("May 24, 2024 00:00:00").getTime();
+let countDownDate = new Date("May 30, 2024 00:00:00").getTime();
 let x = setInterval(function () {
   let now = new Date().getTime();
   let distance = countDownDate - now;
