29ffb6724fd25e31d49a943047ff251052adf63e
Bia Grigore <biancag.grigore@gmail.com>
2024-03-25 20:21:47 +0200

    increment, decrement, update, calculation, local storage, JSON

--- shop.js
+++ shop.js
@@ -31,12 +31,7 @@ let shopItemsData = [
 ]; // when we wrote in html, we wrote with the hand.  we are going to automate it
 // we are going to make an array;
 // we are storing object inside the array {}
-let basket = [
-  {
-    id: "jdfkjhdfkdf",
-    item: 1,
-  },
-]; //whicg items we selected
+let basket = JSON.parse(localStorage.getItem("data")) || []; //which items we selected,it s gonna store in the basket
 let generateShop = () => {
   //arrow function
   //generating the cart using javascript
@@ -67,18 +62,89 @@ let generateShop = () => {
 generateShop();
 //increment function
 let increment = (id) => {
-  let selectedItem = id;
+  let selectedItem = id; //we are gonna pass the id here
+  let search = basket.find(function (y) {
+    //--EXEMPLU INCEPUT--:const inventory = [
+    //{ name: "apples", quantity: 2 },
+    // { name: "bananas", quantity: 0 },
+    // { name: "cherries", quantity: 5 },
+    // ];
 
-  basket.push({
-    id: selectedItem.id,
-    item: 1,
+    //function isCherries(fruit) {
+    //return fruit.name === "cherries";
+    //}
+    //---EXEMPLU TERMINAT--
+    //aici am folosit functie traditionala
+    return y.id === selectedItem.id; //its gonna search the object that you selected;
   });
-  console.log(basket);
+  //y is gonna check all the objects one by one
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  localStorage.setItem("data", JSON.stringify(basket)); //i am setting data inside the local storage;ii dam numele de data(key);basket the object that is getting stored
+  //console.log(basket);
+  update(selectedItem.id); //am apelat functia update
 };
+
 //decrement function
 let decrement = (id) => {
   let selectedItem = id;
-  console.log(selectedItem.id);
+  let search = basket.find((y) => {
+    // y-its gonna check all the object one by one
+    // aici am folosit arrow function
+    return y.id === selectedItem.id; //its gonna search the object that you selected; tha think exists or not
+  });
+  //y is gonna check all the objects one by one
+  if (search.item === 0)
+    return; // se opreste la 0, asta ca sa nu imi dea cu -1,-2,-3 etc.
+  else {
+    search.item -= 1;
+  }
+  localStorage.setItem("data", JSON.stringify(basket));
+  //console.log(basket);
+  update(selectedItem.id); //la fel si aici am apelat functia update;
 };
 //update function
-let update = () => {};
+let update = (id) => {
+  // defiecare data cand apasam plus sau minus, adaugam la rezultatul din mijloc valoare
+  let search = basket.find(function (x) {
+    return x.id === id; // ultimul id e acealasi cu parametrull id; It compares the id of the current element x with the id passed to the update function.
+  });
+  //console.log(search.item);
+  document.getElementById(id).innerHTML = search.item; //asa adaugam numerele in pagina web
+  calculation(); // o apelam pt ca atunci cand functioa update se trigaruieste, sa trigaruieste si  let calculation
+};
+//FUNCTIA DE ADAUGARE A SUMEI IN COS
+//let calculation = () => {
+// let cardIcon = document.getElementById("cartAmount");
+///VARIANTA CU NORMAL FUNCTION----
+// let search = basket.map(function (x) {
+//am folosit ma ca sa mi itereze si sa mi puna toata suma intr un array
+//return x.item; //i am dat return x.item ca sa mi dea doar itemul adica numarul
+// });
+//let reduce = basket.reduce(function (x, y) {
+// return x + y, 0; //0- is the default. number- adica calculul incepe de la 0
+//});
+// console.log(search, reduce);
+//};
+//x and y -one of them is the previous number and the other one is the next number
+// ----STOP NORMAL FUNCTION---
+//---ARROW FUNCTION---
+let calculation = () => {
+  let cardIcon = document.getElementById("cartAmount");
+  let suma = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+  cardIcon.innerHTML = suma;
+};
+//-- if we use local storage we can save the data; daca dam refresh la pagina, suma din cos nu dispare
+//when we want to trigger the local storage?
+//-when we press plus minus etc
+//folosim localStorage in codul de mai sus
+// am folost JSON.stringfy
+//we have to retrieve(a recupera) the data from the local storage into our application
+// asa ca scriem in dreptul lui basket let basket = .JSON.parse(localStorage.getItem('data')) || [],
+//inlocuim basket=[], cu ce am zis in randul de mai sus;scriem cu || [], in caz ca nu avem date, atunci va da eroare;asa ca sa impiedicam asta scriem || []
