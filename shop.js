b9f016a5baf2628d5751164039728dbc318f21bc
Bia Grigore <biancag.grigore@gmail.com>
2024-03-28 20:17:24 +0200

    adaugare poze in shop, mofidicari la pozele din html si rezolvat problema cu cannot get

--- shop.js
+++ shop.js
@@ -1,32 +1,144 @@
 let shop = document.getElementById("shop");
 let shopItemsData = [
   {
-    id: "unu",
+    id: "one",
     name: "Colorful rug",
     price: 80,
     desc: "made from yarn",
     img: "./photos/RolsStrata05_1400x.webp",
   },
   {
-    id: "doi",
+    id: "two",
     name: "Green pillow",
     price: 30,
     desc: "made of 100% linen",
     img: "./photos/Bungalow_pude_seagrass_LINEN_50x50_08a17a8b-057f-4c30-9259-ea31c2efc54a_1400x.webp",
   },
   {
-    id: "trei",
+    id: "three",
     name: "Yellow pillow",
     price: 30,
     desc: "made from yarn",
     img: "./photos/Bungalow_pude_ochre_LINEN_50x50_1f841a54-f9cd-4a70-9138-a498c92331bf_1400x.webp ",
   },
   {
-    id: "patru",
+    id: "four",
     name: "Apron",
     price: 25,
     desc: "made of 100% cotton",
     img: "./photos/Vearkforklaedebla_1400x.webp",
+  },
+  {
+    id: "five",
+    name: "Daily journal notebook",
+    price: 14,
+    desc: "material: paper",
+    img: "./photos/journal2.webp",
+  },
+  {
+    id: "six",
+    name: "Daily journal notebook",
+    price: 14,
+    desc: "material: paper",
+    img: "./photos/journal1.webp",
+  },
+  {
+    id: "seven",
+    name: "Daily journal notebook",
+    price: 14,
+    desc: "material: paper",
+    img: "./photos/journal3.webp",
+  },
+  {
+    id: "eight",
+    name: "Daily journal notebook",
+    price: 14,
+    desc: "material: ceramic",
+    img: "./photos/journal4.webp",
+  },
+  {
+    id: "nine",
+    name: "Plate 1",
+    price: 10,
+    desc: "material: ceramic",
+    img: "./photos/fish3.webp",
+  },
+  {
+    id: "ten",
+    name: "Plate 2",
+    price: 10,
+    desc: "material: ceramic",
+    img: "./photos/fish1.webp",
+  },
+  {
+    id: "twelve",
+    name: "Plate 3",
+    price: 10,
+    desc: "material: ceramic",
+    img: "./photos/fish2.webp",
+  },
+  {
+    id: "twelve",
+    name: "Plate 4",
+    price: 10,
+    desc: "material: paper",
+    img: "./photos/fish4.webp",
+  },
+  {
+    id: "twelve",
+    name: "Penitent Magdalene ",
+    price: 5,
+    desc: "material: paper",
+    img: "./photos/penitent.jpg",
+  },
+  {
+    id: "twelve",
+    name: " Arnolfini portrait",
+    price: 5,
+    desc: "material: paper",
+    img: "./photos/green.jpg",
+  },
+  {
+    id: "Lady ",
+    name: " Lady with an Ermin",
+    price: 5,
+    desc: "material: paper",
+    img: "./photos/girl.jpg",
+  },
+  {
+    id: "twelve",
+    name: "Mona Lisa",
+    price: 5,
+    desc: "material: paper",
+    img: "./photos/monalisa.jpg",
+  },
+  {
+    id: "twelve",
+    name: "The boy in red vest",
+    price: 5,
+    desc: "material: paper",
+    img: "./photos/boy in red vest.jpg",
+  },
+  {
+    id: "twelve",
+    name: "The storm on the sea ",
+    price: 5,
+    desc: "material: paper",
+    img: "./photos/boat.jpg",
+  },
+  {
+    id: "twelve",
+    name: "The swing",
+    price: 5,
+    desc: "material: paper",
+    img: "./photos/the swing.jpg",
+  },
+  {
+    id: "twelve",
+    name: "People",
+    price: 5,
+    desc: "material: paper",
+    img: "./photos/50_028_64540.jpg",
   },
 ]; // when we wrote in html, we wrote with the hand.  we are going to automate it
 // we are going to make an array;
@@ -43,7 +155,7 @@ let generateShop = () => {
       //am pus ${x.name,img etc} pt a schimba la fiecare obiect valorile
       let search1 = basket.find((x) => x.id === id) || [];
       return `<div id=product-id-${id} class="item"> 
-    <img width="200" src="${img}" />
+    <img width="200" height='200' src="${img}" />
     <div class="details">
       <h3>${name}</h3> 
       <h3>${desc}</h3>
