f2577e5d865300969f7ec9190ea787ec0d8c9139
Bia Grigore <biancag.grigore@gmail.com>
2024-04-02 11:01:03 +0300

    inchidere prin escape

--- main.js
+++ main.js
@@ -55,4 +55,16 @@ const closeModal = function () {
   overlay.classList.add("hidden");
 };
 closeModalBtn.addEventListener("click", closeModal);
+let escapeModal = function (event) {
+  //This anonymous function takes one parameter event, which represents the event object.
+  // Inside the function, it checks if the pressed key is the "Escape" key.
+  //If the pressed key is "Escape", it calls the closeModal function.
+  if (event.key === "Escape") {
+    closeModal();
+  }
+};
+document.addEventListener("keydown", escapeModal);
+//This line adds an event listener to the document object.
+//It listens for a "keydown" event anywhere on the document.
+//When a key is pressed, it triggers the escapeModal function.
 //
